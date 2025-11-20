ca4a6103dc146cf6e25147a97ac9e95258cc6a7e
RafaUchoa <rafa.uchoa@gmail.com>
2025-11-20 18:44:40 -0300

    execicios 1844

---
+++ Exercicios/B_Operadores/PrecedenciaOperadoresAtribuicao.java
@@ -0,0 +1,28 @@
+//package Exercicios.B_Operadores;
+
+public class PrecedenciaOperadoresAtribuicao {
+
+    public static void main(String[] args){
+        int numero = 5;
+        int incremento = 0;
+        int decremento = 0;
+
+        System.out.println("Valores iniciais -> incremento: " + incremento + ", Numero: " + numero);
+
+        incremento = ++numero;
+        System.out.println("incremento = ++numero");
+        System.out.println("Incremento: " + incremento + ", Numero: " + numero);
+
+        incremento = numero++;
+        System.out.println("incremento = numero++");
+        System.out.println("Incremento: " + incremento + ", Numero: " + numero);
+
+        decremento = --numero;
+        System.out.println("decremento = --numero");
+        System.out.println("Decremento: " + decremento + ", Numero: " + numero);
+
+        decremento = numero--;
+        System.out.println("decremento = numero--");
+        System.out.println("Decremento: " + decremento + ", Numero: " + numero);
+    }
+}
