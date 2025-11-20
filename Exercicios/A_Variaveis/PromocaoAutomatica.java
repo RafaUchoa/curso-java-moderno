97fbfbcfb7c1db78d1edbaa72e13c2204940e7fd
RafaUchoa <rafa.uchoa@gmail.com>
2025-11-20 18:13:41 -0300

    execicios 1813

---
+++ Exercicios/A_Variaveis/PromocaoAutomatica.java
@@ -0,0 +1,18 @@
+public class PromocaoAutomatica {
+
+    public static void main(String[] args){
+        byte codigo = 10;
+        short quantidade = 50;
+        char tamanho = 'M';
+        int precoUnitario = 200;
+        long codigoBarra = 1234567890123L;
+        float desconto = 15.5f;
+        double precoFinal = 17.5;
+
+        int resultado = codigo + quantidade;
+
+        System.out.println("Resultado da promoção automática: " + resultado);
+        System.out.println("Tamanho do produto: " + tamanho);
+    }
+    
+}
---
+++ Exercicios/B_Operadores/OperadoresAtribuicaoExercicio.java
@@ -0,0 +1,21 @@
+package Exercicios.B_Operadores;
+
+public class OperadoresAtribuicaoExercicio {
+        
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println("Valor inicial: " + numero);
+
+        numero += 1;
+        System.out.println("Após += 1: " + numero);
+        
+        numero -= 2;
+        System.out.println("Após -= 2: " + numero);
+
+        numero *= 3;
+        System.out.println("Após *= 3: " + numero);
+
+        numero /= 2;
+        System.out.println("Após /= 2: " + numero);
+    }
+}
