b67bd5148b70bbfdf394ce0c68685998aa72ec44
RafaUchoa <rafa.uchoa@gmail.com>
2025-11-20 11:05:10 -0300

    execicio variaveis 1105

---
+++ Exercicios/A_Variaveis/ControleEstoquePadaria.java
@@ -0,0 +1,10 @@
+public class ControleEstoquePadaria{
+    
+    public static void main(String[] args) {
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+
+        System.out.println("Quantidade de farinha em estoque: " + quantidadeFarinha);
+        System.out.println("Quantidade de açúcar em estoque: " + quantidadeAcucar);
+    }
+}
